e9d0df77b8b6f31158a9a216814fd7c5423cbdfa
Tyrannas <someonefefe@gmail.com>
2015-12-06 22:44:19 +0100

    Menu fonctionnel
    (attention, remplacer le path par l'emplacement local, manque le chemin
    relatif)

--- menu.lisp
+++ menu.lisp
@@ -1,4 +1,8 @@
-(setq path (merge-pathnames "C:\Users\Felix\Documents\GitHub\Syst-meExpertIA01\door.txt"))
-(with-open-file (input p)
-   (loop for line = (read-line input nil)
-       while line do (format t "~a~%" line)))
+(setq path (merge-pathnames "C:/Users/Felix/Documents/GitHub/Syst-meExpertIA01/door.txt"))
+
+
+(let ((in (open path :if-does-not-exist nil)))
+  (when in
+    (loop for line = (read-line in nil)
+         while line do (format t "~a~%" line))
+    (close in)))
