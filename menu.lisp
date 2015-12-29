4556b3b1dc7cd9cb390393b30eabc1db71eb6a09
oiseauroch <tobollive@aol.com>
2015-12-29 17:31:34 +0100

    lieux terre du milieu

--- menu.lisp
+++ menu.lisp
@@ -1,5 +1,5 @@
-(setq path (merge-pathnames "C:/Users/Felix/Documents/GitHub/Syst-meExpertIA01/door.txt"))
-
+(setq path (merge-pathnames "/home/oiseauroch/Syst-meExpertIA01/door.txt"))
+;; (setq path (merge-pathnames "C:/Users/Felix/Documents/GitHub/Syst-meExpertIA01/door.txt"))
 
 (let ((in (open path :if-does-not-exist nil)))
   (when in
